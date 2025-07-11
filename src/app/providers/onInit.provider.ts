df1d422438ab31a94a190cfadc9ab7d03e2c93b8
kalush666 <jonathan.kalush@gmail.com>
2025-07-11 23:07:15 +0300

    on init

---
+++ src/app/providers/onInit.provider.ts
@@ -0,0 +1,33 @@
+import { Provider, APP_INITIALIZER } from '@angular/core';
+import { UserCookieApiService } from '../api/userCookieApi.service';
+import { UserService } from '../services/user.service';
+
+export function onInit(
+  userCookieApi: UserCookieApiService,
+  userService: UserService
+) {
+  return () => {
+    const cookie = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('token='))
+      ?.split('=')[1];
+    if (cookie) {
+      return userCookieApi
+        .getUserCookie(cookie)
+        .toPromise()
+        .then((data) => {
+          if (data?.userDetails?.userName) {
+            userService.setUserName(data.userDetails.userName);
+          }
+        });
+    }
+    return Promise.resolve();
+  };
+}
+
+export const OnInitProvider: Provider = {
+  provide: APP_INITIALIZER,
+  useFactory: onInit,
+  deps: [UserCookieApiService, UserService],
+  multi: true,
+};
