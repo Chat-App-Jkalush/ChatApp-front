ff02e53a22b6c674dd384ea4a35277cd723f0524
kalush666 <jonathan.kalush@gmail.com>
2025-07-13 11:41:29 +0300

    base compoent

--- src/app/components/home/child components/show-chat/show-chat.component.ts
+++ src/app/components/home/child components/show-chat/show-chat.component.ts
@@ -4,10 +4,14 @@ import {
   Input,
   OnChanges,
   SimpleChanges,
+  ViewChild,
+  ElementRef,
+  AfterViewChecked,
 } from '@angular/core';
 import { ChatApiService } from '../../../../api/chatApi.service';
 import { RefreshDataService } from '../../../../services/refreshData.service';
 import { messageInfoResponse } from '../../../../../../../common/Ro/message.ro';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-show-chat',
@@ -15,35 +19,73 @@ import { messageInfoResponse } from '../../../../../../../common/Ro/message.ro';
   templateUrl: './show-chat.component.html',
   styleUrls: ['./show-chat.component.scss'],
 })
-export class ShowChatComponent implements OnInit, OnChanges {
+export class ShowChatComponent implements OnInit, OnChanges, AfterViewChecked {
   @Input() chat: any;
+  @ViewChild('messagesContainer', { static: false })
+  messagesContainer!: ElementRef;
+
   latestChatId: string | null = null;
   userName: string = '';
-  messages: messageInfoResponse[] = [];
+  message!: FormGroup;
+  private shouldScrollToBottom = false;
 
   constructor(
     private chatApi: ChatApiService,
-    private refreshDataService: RefreshDataService
+    private refreshDataService: RefreshDataService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit() {
+    this.message = this.fb.group({
+      content: ['', Validators.required],
+      chatId: [this.chat?.chatId || ''],
+      sender: [this.userName],
+    });
+
     this.refreshDataService.latestChatId$.subscribe((chatId) => {
       this.latestChatId = chatId;
       if (chatId && !this.chat) {
         this.chatApi.getChatById(chatId).subscribe((chat) => {
           this.chat = chat;
+          this.shouldScrollToBottom = true;
         });
       }
     });
 
     this.refreshDataService.userName$.subscribe((userName) => {
       this.userName = userName;
+      this.message.patchValue({ sender: userName });
     });
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['chat'] && this.chat?.chatId) {
       this.refreshDataService.setLatestChatId(this.chat.chatId);
+      this.message.patchValue({ chatId: this.chat.chatId });
+      this.shouldScrollToBottom = true;
     }
+  }
+
+  ngAfterViewChecked() {
+    if (this.shouldScrollToBottom) {
+      this.scrollToBottom();
+      this.shouldScrollToBottom = false;
+    }
+  }
+
+  private scrollToBottom(): void {
+    if (this.messagesContainer) {
+      try {
+        this.messagesContainer.nativeElement.scrollTop =
+          this.messagesContainer.nativeElement.scrollHeight;
+      } catch (err) {
+        console.error('Error scrolling to bottom:', err);
+      }
+    }
+  }
+
+  sendMessage() {}
+  trackByMessage(index: number, message: any): any {
+    return message.id || index;
   }
 }
