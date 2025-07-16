11abaf4a557f6106809bb77ce32c159a1942eccd
kalush666 <jonathan.kalush@gmail.com>
2025-07-16 09:15:00 +0300

    is online

--- src/app/services/chatSocket.service.ts
+++ src/app/services/chatSocket.service.ts
@@ -8,29 +8,54 @@ import {
 @Injectable({ providedIn: 'root' })
 export class ChatSocketService {
   private socket: Socket;
+  private joinedChats = false;
+  private eventListeners = new Map<string, Function[]>();
 
   constructor() {
     this.socket = io(DEFAULT_PORT_ORIGIN, {
       transports: ['websocket'],
       withCredentials: true,
     });
-    this.socket.on('connect', () => {});
-    this.socket.on('disconnect', (reason: string) => {});
-    this.socket.onAny((event, ...args) => {});
+
+    this.socket.on('connect', () => {
+      console.log('Socket connected');
+      this.joinedChats = false;
+    });
+
+    this.socket.on('disconnect', (reason: string) => {
+      console.log('Socket disconnected:', reason);
+      this.joinedChats = false;
+    });
+
+    this.socket.onAny((event, ...args) => {
+      console.log('Socket event:', event, args);
+    });
   }
 
   getSocket(): Socket {
     return this.socket;
   }
 
-  joinChats(userName: string) {
-    if (this.socket && this.socket.connected) {
-      this.socket.emit(EVENTS.JOIN_CHAT, { userName });
-    } else {
-      this.socket.once('connect', () => {
+  joinChats(userName: string): Promise<void> {
+    return new Promise((resolve) => {
+      if (this.joinedChats) {
+        resolve();
+        return;
+      }
+
+      const doJoin = () => {
         this.socket.emit(EVENTS.JOIN_CHAT, { userName });
-      });
-    }
+        this.joinedChats = true;
+        console.log(`User ${userName} joined chats`);
+        resolve();
+      };
+
+      if (this.socket && this.socket.connected) {
+        doJoin();
+      } else {
+        this.socket.once('connect', doJoin);
+      }
+    });
   }
 
   sendMessage(message: any) {
@@ -44,16 +69,86 @@ export class ChatSocketService {
   }
 
   onEvent(event: string, callback: (...args: any[]) => void) {
+    if (!this.eventListeners.has(event)) {
+      this.eventListeners.set(event, []);
+    }
+    this.eventListeners.get(event)!.push(callback);
+
     this.socket.on(event, (...args) => {
       callback(...args);
     });
+
+    console.log(
+      `Registered listener for event: ${event}. Total listeners: ${
+        this.eventListeners.get(event)!.length
+      }`
+    );
+  }
+
+  removeListener(event: string, callback: Function) {
+    this.socket.off(event, callback as any);
+
+    if (this.eventListeners.has(event)) {
+      const listeners = this.eventListeners.get(event)!;
+      const index = listeners.indexOf(callback);
+      if (index > -1) {
+        listeners.splice(index, 1);
+      }
+      console.log(
+        `Removed listener for event: ${event}. Remaining listeners: ${listeners.length}`
+      );
+    }
+  }
+
+  removeAllListeners(event: string) {
+    this.socket.removeAllListeners(event);
+    this.eventListeners.delete(event);
+    console.log(`Removed all listeners for event: ${event}`);
   }
 
   disconnect() {
     this.socket.disconnect();
+    this.joinedChats = false;
+    this.eventListeners.clear();
   }
 
   leaveChat(chatId: string, userName: string) {
     this.socket.emit(EVENTS.LEAVE_CHAT, { chatId, userName });
+  }
+
+  isOnline(userName: string): Promise<boolean> {
+    return new Promise((resolve) => {
+      const timeout = setTimeout(() => {
+        console.log(`Timeout checking if ${userName} is online`);
+        resolve(false);
+      }, 5000);
+
+      this.socket.emit(EVENTS.IS_ONLINE, { userName });
+      this.socket.once('isOnlineResult', (data) => {
+        clearTimeout(timeout);
+        console.log(`${userName} is ${data.isOnline ? 'online' : 'offline'}`);
+        resolve(data.isOnline);
+      });
+    });
+  }
+
+  getOnlineContacts(contacts: string[]): Promise<string[]> {
+    return new Promise((resolve) => {
+      const timeout = setTimeout(() => {
+        console.log('Timeout getting online contacts');
+        resolve([]);
+      }, 5000);
+
+      this.socket.emit(EVENTS.GET_ONLINE_USERS, { contacts });
+      this.socket.once('onlineUsersList', (data) => {
+        clearTimeout(timeout);
+        console.log('Online contacts received:', data.onlineContacts);
+        resolve(data.onlineContacts);
+      });
+    });
+  }
+
+  getListenerCount(event: string): number {
+    return this.eventListeners.get(event)?.length || 0;
   }
 }
